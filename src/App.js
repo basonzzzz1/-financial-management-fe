6c6d50d89c3bb12e844d682a30677fddc048c458
basonzzzz1 <132861064+basonzzzz1@users.noreply.github.com>
2023-12-13 21:52:47 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import {ToastContainer} from "react-toastify";
+import Login from "./component/Login";
+import {Provider} from "react-redux";
+import store from "./redux/store/store";
+import Home from "./component/Home";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Provider store={store}>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Navigate to="/login"/>}/>
+                <Route path={"/login"} element={<Login/>}/>
+                <Route path={"/home"} element={<Home/>}/>
+            </Routes>
+            <ToastContainer/>
+        </BrowserRouter>
+        </Provider>
     </div>
   );
 }
---
+++ src/component/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div style={{backgroundColor : "red"}}>
+            hôm
+        </div>
+    );
+};
+
+export default Home;
